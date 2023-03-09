2218805781d0f3da4b4dde0ae026e8193cf8f5a1
AliRn <alirn1997@gmail.com>
2023-03-10 01:37:26 +0330

    Make it ready for publish

---
+++ pantherdb/__init__.py
@@ -0,0 +1,6 @@
+from pantherdb.pantherdb import *
+
+__version__ = '1.1.0'
+
+
+__all__ = ('__version__', 'PantherDB', 'PantherCollection', 'PantherDocument', 'PantherDBException')
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+from setuptools import setup
+
+
+def pantherdb_version() -> str:
+    with open(os.path.join('pantherdb/__init__.py')) as f:
+        return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
+
+
+VERSION = pantherdb_version()
+DESCRIPTION = open('README.md').read()
+
+setup(
+    name='pantherdb',
+    version=VERSION,
+    python_requires='>=3.11',
+    author='Ali RajabNezhad',
+    author_email='alirn76@yahoo.com',
+    url='https://github.com/alirn76/pantherdb',
+    description='is a Simple, FileBase and Document Oriented database',
+    long_description=DESCRIPTION,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+    install_requires=[
+        'orjson>=3.8.6',
+    ],
+)
