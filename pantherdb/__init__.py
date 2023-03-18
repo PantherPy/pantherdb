f19ecc6743f9c307dd98a4cccf5148819a3cde8f
AliRn <alirn1997@gmail.com>
2023-03-18 22:32:00 +0330

    Fix a mistake :)

--- pantherdb/__init__.py
+++ pantherdb/__init__.py
@@ -1,6 +1,6 @@
 from pantherdb.pantherdb import *
 
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 
 
 __all__ = ('__version__', 'PantherDB', 'PantherCollection', 'PantherDocument', 'PantherDBException')
