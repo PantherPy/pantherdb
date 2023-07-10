0701152d84ea58e2288251efeeb801b995861031
AliRn <alirn1997@gmail.com>
2023-07-10 13:49:38 +0330

    Trying to support python3.12

--- pantherdb/__init__.py
+++ pantherdb/__init__.py
@@ -1,6 +1,6 @@
 from pantherdb.pantherdb import *
 
-__version__ = '1.2.6'
+__version__ = '1.2.7'
 
 
 __all__ = ('__version__', 'PantherDB', 'PantherCollection', 'PantherDocument', 'PantherDBException')
